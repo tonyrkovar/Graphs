2b65b9ec3597ae3f53a95bf88b9189c0a7b94124
Tony <tonyrkovar@gmail.com>
2020-03-20 01:09:42 -0500

    WIP: working on adv, need rest to clean up

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -25,11 +25,12 @@ class Stack():
     def size(self):
         return len(self.stack)
 
+
     # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 
@@ -50,47 +51,47 @@ traversal_path = []
 def room_traversal(player):
     s = Stack()
     s.push(player.current_room)
-    # Using a dictionary to track the room paths, and the values will be an array or arrays where
-    # i'll return the shortest path in each room?
-    paths_from_room = {}
-    for room in room_graph:
-        paths_from_room[room] = room_graph[room][1]
 
     visited = {}
 
     current_path = []
-    print(len(room_graph))
+    # print(len(room_graph))
+
+    def traverse(move):
+        traversal_path.append(move)
+        player.travel(move)
+        s.push(player.current_room)
+        visited[cr_id][move] = player.current_room.id
+        last_move = move
+        # print(last_move, 'LAST MOVE ')
 
     while s.size() > 0:
         current_room = s.pop()
         current_exits = current_room.get_exits()
         cr_id = current_room.id
-        # visited[current_room.id] = room_graph[room][1]
-        print(current_room.id)
-        if len(current_exits) == 1:
-            traversal_path.append(current_exits[0])
-            player.travel(current_exits[0])
-            s.push(player.current_room)
-            print(traversal_path, 'Traversal path')
-        else:
-            for move in current_exits:
-                if move not in visited:
-                    traversal_path.append(move)
-                    player.travel(move)
-                    s.push(player.current_room)
 
-                    visited[cr_id] =
-                    # visited[cr_id][move] = 'balls'
-                    # print(dir(visited[cr_id]))visited[cr_id]
-        print(visited)
-        # Start at a single room, then you want to pick a direction to move.
-        # If you move north(based off the test loop file) you will reach a dead end after 2 moves.
-        # I need to traverse the rooms in a way that will have me touch every room, atleast once.
-        # What do I track here?
-        # I currently have a dictionary that has all of the room ID's along with
-        #  avalible paths(paths_from_room)
-        # Write a conditional that will check if a room is a dead end
-        # Write a condition/track a variable that will ensure i do not repeat paths.
+        last_move = current_exits[0]
+        if cr_id not in visited:
+            visited[cr_id] = {}
+        if len(current_exits) == 1:
+            traverse(current_exits[0])
+        else:
+            if last_move not in visited[cr_id]:
+                traverse(last_move)
+            else:
+                for move in current_exits:
+                    if move not in visited[cr_id]:
+                        traverse(move)
+
+    print(visited)
+# Start at a single room, then you want to pick a direction to move.
+# If you move north(based off the test loop file) you will reach a dead end after 2 moves.
+# I need to traverse the rooms in a way that will have me touch every room, atleast once.
+# What do I track here?
+# I currently have a dictionary that has all of the room ID's along with
+#  avalible paths(paths_from_room)
+# Write a conditional that will check if a room is a dead end
+# Write a condition/track a variable that will ensure i do not repeat paths.
 
 
 room_traversal(player)
